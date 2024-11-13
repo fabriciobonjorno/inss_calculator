f7df0802e4f125ca765682c3255bd7603854c634
Fabricio Bonjorno <fabricio.bonjorno@gmail.com>
2024-11-13 12:52:19 -0400

    feat(no-issue): Add test for dashboard view

--- spec/factories/dash_users.rb
+++ spec/factories/dash_users.rb
@@ -2,5 +2,8 @@
 
 FactoryBot.define do
   factory :dash_user do
+    email { "teste@teste.com" }
+    password { "12345678" }
+    password_confirmation { "12345678" }
   end
 end
--- spec/requests/dashboard_spec.rb
+++ spec/requests/dashboard_spec.rb
@@ -3,9 +3,15 @@
 require 'rails_helper'
 
 RSpec.describe "Dashboards", type: :request do
+  let(:dash_user) { create(:dash_user) }
+
+  before do
+    sign_in dash_user
+  end
+
   describe "GET /index" do
     it "returns http success" do
-      get "/dashboard/index"
+      get "/dashboard"
       expect(response).to have_http_status(:success)
     end
   end
